1d7cce89433218abeb823da740abab58599b5804
NellyDigital-arch <nellydigital401@gmail.com>
2026-09-10 17:47:46 +0300

    Add mobile navigation toggle functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+// =========================================
+// MOBILE NAVIGATION TOGGLE
+// =========================================
 
+const mainNavigation = document.getElementById("mainNavigation");
+const navbarToggler = document.querySelector(".navbar-toggler");
+
+if (mainNavigation && navbarToggler) {
+
+    mainNavigation.addEventListener("show.bs.collapse", function () {
+        navbarToggler.classList.add("menu-open");
+    });
+
+    mainNavigation.addEventListener("hide.bs.collapse", function () {
+        navbarToggler.classList.remove("menu-open");
+    });
+
+}
